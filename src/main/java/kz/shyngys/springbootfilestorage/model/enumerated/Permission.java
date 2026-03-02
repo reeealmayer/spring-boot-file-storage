d6f8e6763f191ad56e2f82d3b1cfce57f179dfec
rilmayer <yessenbayev.chingis@gmail.com>
2026-03-02 19:33:16 +0500

    - added permissions on roles

---
+++ src/main/java/kz/shyngys/springbootfilestorage/model/enumerated/Permission.java
@@ -0,0 +1,16 @@
+package kz.shyngys.springbootfilestorage.model.enumerated;
+
+public enum Permission {
+    USERS_READ("users:read"),
+    USERS_WRITE("users:write");
+
+    private final String permission;
+
+    Permission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+}
--- src/main/java/kz/shyngys/springbootfilestorage/model/enumerated/UserRole.java
+++ src/main/java/kz/shyngys/springbootfilestorage/model/enumerated/UserRole.java
@@ -1,5 +1,29 @@
 package kz.shyngys.springbootfilestorage.model.enumerated;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public enum UserRole {
-    ADMIN, MODERATOR, USER;
+    ADMIN(Set.of(Permission.USERS_READ, Permission.USERS_WRITE)),
+    MODERATOR(Set.of(Permission.USERS_READ, Permission.USERS_WRITE)),
+    USER(Set.of(Permission.USERS_READ, Permission.USERS_WRITE));
+
+    private final Set<Permission> permissions;
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    UserRole(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
 }
